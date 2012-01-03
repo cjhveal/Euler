bd7e19a400c178058eaabadaede36554a4726f04
Cody Veal <cveal05@gmail.com>
2012-01-02 22:45:25 -0500

    Generalized solution to problem 4

--- 4.hs
+++ 4.hs
@@ -1,1 +1,7 @@
-answer = maximum [x*y | x <- [100..999], y<-[100..999], show (x*y) == reverse (show (x*y))]
+-- Largest palindromic produdt of two n digit numbers
+maxPal :: Int -> Int
+maxPal 0 = 0
+maxPal n = maximum [x*y | x <- domain, y <- domain, show(x*y) == reverse(show(x*y))]
+  where domain = [(10^(n-1))..((10^n)-1)]
+
+answer = maxPal 3
