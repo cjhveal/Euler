1fcc35f545b9076b86ce62728fcce9bdb04ffd06
Cody Veal <cveal05@gmail.com>
2012-01-02 23:06:48 -0500

    Cleaned up problem 9 solutions

--- 9.hs
+++ 9.hs
@@ -1,1 +1,4 @@
-answer = [a*b*c | c <- [1..998], b <- [1..c-1], a <- [1..b-1], a*a + b*b == c*c, a+b+c==1000]
+-- Products of the pythagorean triplets (a,b,c) s.t. a+b+c = n, 
+-- if one exists or [] otherwise.
+prodPythTrip :: Int -> [Int]
+prodPythTrip n = [a*b*c | c <- [1..n-2], b <- [1..c-1], a <- [1..b-1], a*a + b*b == c*c, a+b+c==n]
