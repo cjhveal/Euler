dbc3e173e6ee99f34c1281aab464436e7454b513
Cody Veal <cveal05@gmail.com>
2012-01-02 22:35:45 -0500

    Added type signatures for solution 2

--- 2.hs
+++ 2.hs
@@ -1,10 +1,15 @@
+fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
+
+fib :: Int -> Int
 fib n = fibs!!n
 
+gather_fibs :: [Int]
 gather_fibs = gather_fibs' 0
   where
     gather_fibs' n = case fib n < 4000000 of
       True -> [fib n] ++ gather_fibs' (n+1)
       False -> []
 
+answer :: Int
 answer = sum [x | x <- gather_fibs, x `mod` 2 == 0]
