3f8696f681b3c11548809730ff872b571106e29a
Cody Veal <cveal05@gmail.com>
2012-08-30 10:35:08 -0400

    cleaning up anonymous lambdas everywhere

--- 1.hs
+++ 1.hs
@@ -3,6 +3,6 @@
 multiples :: [Int] -> [Int] -> [Int]
 multiples [] _ = []
 multiples _ [] = []
-multiples xs ys = filter (\x -> foldr (\y-> \z-> z || ( x `mod` y == 0)) False ys) xs
+multiples xs ys = filter (\x -> any (\y-> ( x `mod` y == 0)) ys) xs
 
 answer = sum $ multiples [1..999] [3,5]
--- 6.hs
+++ 6.hs
@@ -1,10 +1,10 @@
 -- Calculates the diference between the the square of the sum of the list
--- and the sum of the squares of the list. 
+-- and the sum of the squares of the list.
 diffSquares :: [Int] -> Int
 diffSquares [] = 0
 diffSquares xs = squareOfSums - sumOfSquares
   where
     squareOfSums = (sum xs)^2
-    sumOfSquares = sum $ map (\x -> x^2) xs
+    sumOfSquares = sum $ map (flip (^) 2) xs
 
 answer = diffSquares [1..100]
