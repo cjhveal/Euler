8ced89d4e36d3667088521741219e1053a622b98
Cody Veal <cveal05@gmail.com>
2012-10-17 16:57:29 -0400

    cleaned p2

--- 2.hs
+++ 2.hs
@@ -4,18 +4,9 @@
 fibs :: [Int]
 fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
--- fib: Takes an integer and returns the nth fibonacci number
-fib :: Int -> Int
-fib n = fibs!!n
-
 -- gather_fibs: Gathers all of the fibonacci numbers < 4000000
--- TODO: This is very ugly... 
-gather_fibs :: [Int]
-gather_fibs = gather_fibs' 0
-  where
-    gather_fibs' n = case fib n < 4000000 of
-      True -> [fib n] ++ gather_fibs' (n+1)
-      False -> []
+gatherFibs :: [Int]
+gatherFibs = takeWhile (<4000000) fibs
 
 answer :: Int
-answer = sum [x | x <- gather_fibs, x `mod` 2 == 0]
+answer = sum [x | x <- gatherFibs, x `mod` 2 == 0]
